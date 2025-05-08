ce55da07b0c9439f8e99a101a8b1c6368ef39059
Adam <github@cognitive.anonaddy.me>
2025-05-08 12:15:54 +0100

    Added title logic but still need to work on appending proper name

--- metadata.py
+++ metadata.py
@@ -2,12 +2,16 @@ import os
 import zipfile
 import xml.etree.ElementTree as ET
 import sqlite3
-from typing import Optional, Union
+from typing import Optional, Union, Tuple
 from fuzzywuzzy import fuzz
 import sys
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+import re
+from word2number import w2n
+
+
 
 
 log_file = open("log.txt", "w")
@@ -48,12 +52,15 @@ def find_metadata(path: str) -> ET.Element:
 
 def get_text(root, tag: str) -> Optional[str]:
     element = root.find(tag)
-    return element.text.strip() if element is not None and element.text else None
+    if element is not None and element.text:
+        return element.text.strip().lower()
+    else:
+        print("No metadata under {tag} tag.")
 
 
 def normalise_publisher(name: str) -> Optional[str]:
     suffixes = ["comics", "publishing", "group", "press", "inc.", "inc", "llc"]
-    tokens = name.lower().replace("&", "and").split()
+    tokens = name.replace("&", "and").split()
     return " ".join([t for t in tokens if t not in suffixes])
 
 
@@ -95,6 +102,10 @@ def parse_month(path) -> Optional[int]:
 def parse_publisher(path) -> Optional[str]:
     return easy_parse(path, "Publisher")
 
+def parse_title(path) -> Optional[str]:
+    return easy_parse(path, "Title")
+
+
 # More advanced parsing that requires extra logic or string matching
 
 def match_publisher(a: str) -> Optional[int]:  # Takes a string from metadata and returns the publisher id that comes from sql publisher table primary key
@@ -111,20 +122,60 @@ def match_publisher(a: str) -> Optional[int]:  # Takes a string from metadata an
     else:
         None
 
+#use Amazing Spider-Man Modern Era Epic Collection: Coming 
+
+# Naming System: [Series_Name] [by_Surname][Start_Year] - [Collection_Type] [Volume_Number] ([date in month/year])
+
+series_overrides = [("tpb", 1), ("omnibus", 2), ("epic collection", 3), ("modern era epic collection", 4)]
+
+def title_parsing(path) -> Optional[Tuple[str, int]]:
+    common_titles = {'volume', 'book', 'collection'}
+    coll_type = None
+    override = False
+    title_raw = parse_title(path)
+    match = re.search(r'volume\s(\d+)|(one|two|three|four|five|six|seven|eight|nine|ten|eleven|twelve|thirteen|fourteen|fifteen)', title_raw)
+
+    if match:
+        title = parse_series(path)
+        vol = match.group(1)
+        if vol.isdigit():
+            vol_num = int(vol)
+        else:
+            vol_num = w2n.word_to_num(vol) #need to deal with vol_num being None
+#need to deal with 'volume being in the title but other stuff around it' 
+    for f in series_overrides:      
+        if f[0] in title:
+            title = title.replace(f[0], "")
+            override = True
+            coll_type = f[1]
+        if coll_type is None:
+            pass #Need to add a function which will defintely find the type of collection
+        elif override == False:
+            print("No usuable title, need user input.") #Need to do something here to fix if no title can be found
 
 
-def title_parsing(path):
-    vague_titles = {'volume 1', 'volume one', 'book one', 'collection', 'tpb', 'omnibus'}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 # Watchdog stuff:
 
 class DownloadsHandler(FileSystemEventHandler):
+
     def on_created(self, event):
         if not event.is_directory:
             file_path = event.src_path
             print(f"New file detected: {file_path}")
             self.handle_new_file(file_path)
-
-    
+            pass
