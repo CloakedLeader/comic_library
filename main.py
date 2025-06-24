ec2f9e8550b9f46adfa7de48a5167c0aa0250647
Adam <github@cognitive.anonaddy.me>
2025-06-24 16:13:04 +0100

    Create layout for homepage.

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-import os
-import time
-import sqlite3
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+from comic_gui.home_page import HomePage
+
+app = QApplication(sys.argv)
+window = QMainWindow()
+home = HomePage()
+window.setCentralWidget(home)
+window.resize(500, 400)
+window.show()
+app.exec()
