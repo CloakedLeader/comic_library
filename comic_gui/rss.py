d0676001fad4a53d466ff3ab34cae465fdd68c34
Adam <github@cognitive.anonaddy.me>
2025-06-30 16:34:15 +0100

    Added function to automatically download comics (WIP)

--- comic_gui/rss.py
+++ comic_gui/rss.py
@@ -1,14 +1,15 @@
 import feedparser
 import requests
 from bs4 import BeautifulSoup
+from playwright.sync_api import sync_playwright
 
 
 def rss_scrape(base_url="https://getcomics.org/feed/", num=6):
     feed = feedparser.parse(base_url)
     entries = []
     entries = [{"title": e.title, "link": e.link, "summary": e.summary} for e in feed.entries]
-    #entries = entries[:num]
-    return entries[num].get("summary")
+    return entries[0]
+    # entries = entries[:num]
     # gui_ready_list = []
     # for i in entries:
     #     title = i["title"]
@@ -53,5 +54,47 @@ def summary_scrape(html_formatted_string) -> str:
         description_paragraphs.append(text)
 
     return "\n\n".join(description_paragraphs)
+
+def download_comic(comic_info: dict) -> None:
+    url = comic_info.get("link")
+    headers = {"User-Agent": "Mozzilla/5.0"}
+
+    reponse = requests.get(url, headers)
+    soup = BeautifulSoup(reponse.content, "html.parser")
+
+    download_links = []
+
+    for button_div in soup.find_all("div", class_="aio-button-center"):
+        link = button_div.find("a", href=True)
+        if link:
+            href = link["href"]
+            title = link.get("title", "").strip()
+            download_links.append((title, href))
+
+    for title, link in download_links:
+        print(f"{title}: {link}")
+
+    return download_links
+
+def download_third_party_links(url):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto(url, timeout=60000)
+
+        page.wait_for_selector("a:has-text('Download'), button:has-text('Download')", timeout=30000)
+        page.click("a:has-text('Download'), button:has-text('Download')")
+
+        page.wait_for_timeout(3000)
+
+        final_url = page.url
+        browser.close()
+        return final_url
+    
+comic_info = rss_scrape()
+links = download_comic(comic_info)
+title, link = links[3]
+third_party_url = download_third_party_links(link)
+print(third_party_url)
 
 
