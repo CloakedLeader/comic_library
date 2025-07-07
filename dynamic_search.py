15619779956f8dec2e7e783d7a016c008df956c1
Adam <adam.perrott@protonmail.com>
2025-07-07 06:26:53 +0100

    feat: add explicit fieldable search for accuracy

--- dynamic_search.py
+++ dynamic_search.py
@@ -1,4 +1,5 @@
 import sqlite3
+import re
 from typing import Optional
 
 def create_virtual_table() -> None:
@@ -81,3 +82,93 @@ def index_comic_for_search(comic_id: int) -> None:
     conn.commit()
     conn.close()
     return None
+
+def search_comics(user_input: str, limit: int = 20) -> Optional[dict[str, str | int]]:
+    """
+    Search the fts5 database for directly matching reslts.
+
+    Args:
+    user_input: A string prefixed with a field to search in.
+    limit: The maximum number of matching results to be returned.
+
+    Returns:
+
+    """
+    
+    query = parse_search_query(user_input)
+    if not query:
+        return None
+
+    conn = sqlite3.connect("comics.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT rowid FROM comic_search
+        WHERE comic_search MATCH ?
+        LIMIT ?
+    """, (query, limit))
+
+    comic_ids = [row[0] for row in cursor.fetchall()]
+    if not comic_ids:
+        return None
+
+    placeholders = ",".join("?" for _ in comic_ids)
+
+    cursor.execute(f"""
+        SELECT comics.*, publishers.name AS publisher_name,
+        FROM comics
+        LEFT JOIN publishers ON comics.publisher_id = publisher.id
+        WHERE comics.id IN ({placeholders})
+    """, comic_ids)
+
+    results = []
+    colomns = [col[0] for col in cursor.description]
+    for row in cursor.fetchall():
+        results.append(ict(zip(colomns, row)))
+
+
+    return results
+
+
+def parse_search_query(user_input: str) -> str:
+    """
+    Takes a search query and splits it up into
+    the different fields of which it involves.
+
+    Args:
+    user_input: A string of one or more queries 
+    prefixed by a field indicator from field_aliases.
+
+    Returns:
+    A string which clearly declares the query data
+    and their corresponding fields.
+
+    Example::
+    input: '#mutant &marvel char:wolverine creator:claremont"'
+    returns:
+    'tags:"mutant" AND character:"wolverine AND 
+    creators:"claremont"'
+    """
+    field_aliases = {
+        "#": "tags",
+        "&": "publisher",
+        "char:": "characters",
+        "cre::": "creators",
+        "ser::": "series",
+        "gen:": "genre"
+    }
+
+    tokens = user_input.strip().split()
+    terms = []
+    
+    for token in tokens:
+        matched = False
+        for prefix, field in field_aliases.items():
+            if token.lower().startswith(prefix):
+                value = token[len(prefix):]
+                terms.append(f'{field}: "{value}"')
+                matched = True
+                break
+        if not matched:
+            terms.append(f'title: "{token}"')
+    return ' AND '.join(terms)
