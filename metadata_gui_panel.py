223a7497ea04232bd541f1ff4377e687c1f0157b
Adam <github@cognitive.anonaddy.me>
2025-07-08 17:15:54 +0100

    refactor: change image loading class to output pixmap

--- metadata_gui_panel.py
+++ metadata_gui_panel.py
@@ -1,15 +1,25 @@
-from typing import TYPE_CHECKING
-
 from PySide6.QtWidgets import QDialog, QLabel, QPushButton, QVBoxLayout
 
-if TYPE_CHECKING:
-    from reader import SimpleReader
+from reader import Comic
 
 
 class MetadataDialog(QDialog):
-    def __init__(self, reader: SimpleReader) -> None:
+    """
+    A dialog window for displaying comic metadata information.
+    """
+
+    def __init__(self, comic: Comic) -> None:
+        """
+        Initalise the metadata dialog.
+
+        Args:
+            reader: A SimpleReader instance containing comic data and filename.
+
+        This dialog shows metadata fields for a given comic reader instance
+        and provides a close button for user interaction.
+        """
         super().__init__()
-        self.setWindowTitle(f"Metadata for {reader.filename}")
+        self.setWindowTitle(f"Metadata for {comic.filename}")
 
         layout = QVBoxLayout()
         layout.addWidget(QLabel("Here you will see all the different metadata fields."))
