523db035e6a71228d0d1d6491fa1a7f2e9583f00
Adam Perrott <adam.perrott@protonmail.com>
2026-01-21 17:54:29 +0000

    feat: update test_lexer_and_parser to follow new datatype

--- tests/test_lexer_and_parser.py
+++ tests/test_lexer_and_parser.py
@@ -14,25 +14,25 @@ def lex_and_parse(filename: str):
 
 def test_primitive():
     result = lex_and_parse("Batman No Man's Land (2015)")
-    assert result["year"] == 2015
-    assert result["title"] == "Batman No Man's Land".lower()
+    assert result.year == 2015
+    assert result.title == "Batman No Man's Land".lower()
 
 
 def test1():
     result = lex_and_parse("Batman Omnibus Vol 1 (2019)")
 
-    assert result["collection"] == "omnibus"
-    assert result["year"] == 2019
-    assert result["volume"] == 1
+    assert result.collection_type== "omnibus"
+    assert result.year == 2019
+    assert result.volume_number == 1
 
 
 def test2():
     result = lex_and_parse("Batman v3 162 (2026) (Webrip) (The Last Kryptonian-DCP)")
 
-    assert result["year"] == 2026
-    assert result["issue"] == 162
-    assert result["volume"] == 3
-    assert result["series"] == "batman" or result["title"] == "batman"
+    assert result.year == 2026
+    assert result.issue_number == 162
+    assert result.volume_number == 3
+    assert result.series == "batman" or result.title == "batman"
 
 
 def test3():
@@ -40,10 +40,10 @@ def test3():
         "Spider-Boy - Full Circle v01 (2026) (digital) (Marika-Empire)"
     )
 
-    assert result["title"] == "full circle"
-    assert result["series"] == "spider-boy"
-    assert result["year"] == 2026
-    assert result["volume"] == 1
+    assert result.title == "full circle"
+    assert result.series == "spider-boy"
+    assert result.year == 2026
+    assert result.volume_number == 1
 
 
 def test4():
@@ -51,10 +51,10 @@ def test4():
         "Guardians of the Galaxy v01 - Cosmic Avengers (2013) (Digital) (F) (Zone-Empire)"
     )
 
-    assert result["series"] == "guardians of the galaxy"
-    assert result["title"] == "cosmic avengers"
-    assert result["volume"] == 1
-    assert result["year"] == 2013
+    assert result.series == "guardians of the galaxy"
+    assert result.title == "cosmic avengers"
+    assert result.volume_number == 1
+    assert result.year == 2013
 
 
 def test5():
@@ -62,10 +62,10 @@ def test5():
         "Justice League - The Atom Project (2025) (digital) (Son of Ultron-Empire)"
     )
 
-    assert result["series"] == "justice league"
-    assert result["title"] == "the atom project"
-    assert result["volume"] == 1
-    assert result["year"] == 2025
+    assert result.series == "justice league"
+    assert result.title == "the atom project"
+    assert result.volume_number == 1
+    assert result.year == 2025
 
 
 def test6():
@@ -73,10 +73,10 @@ def test6():
         "Moon Knight - Fist Of Khonshu - Subterranean Jungle v01 (2026) (digital) (Marika-Empire)"
     )
 
-    assert result["volume"] == 1
-    assert result["year"] == 2026
-    assert result["series"] == "moon knight fist of khonshu"
-    assert result["title"] == "subterranean jungle"
+    assert result.volume_number == 1
+    assert result.year == 2026
+    assert result.series == "moon knight fist of khonshu"
+    assert result.title == "subterranean jungle"
 
 
 def test7():
@@ -85,8 +85,8 @@ def test7():
     )
 
     assert (
-        result["series"] == "doctor strange of asgard"
-        or result["title"] == "doctor strange of asgard"
+        result.series == "doctor strange of asgard"
+        or result.title == "doctor strange of asgard"
     )
-    assert result["volume"] == 1
-    assert result["year"] == 2026
+    assert result.volume_number == 1
+    assert result.year == 2026
